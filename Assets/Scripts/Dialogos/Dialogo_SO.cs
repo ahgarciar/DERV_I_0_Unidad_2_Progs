d48322c87f1a026bd1dae4a64725844ca4252e23
ahgarciar <ahgarcia@docentes.uat.edu.mx>
2021-10-18 14:58:28 -0500

    Clase 18 Oct

--- Assets/Scripts/Dialogos/Dialogo_SO.cs
+++ Assets/Scripts/Dialogos/Dialogo_SO.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,6 +15,15 @@ public class Dialogo_SO : MonoBehaviour
     public GameObject contenedorImagen;
     public Image image;
 
+    [SerializeField]
+    public GameObject contenedorDialogo;
+
+    [SerializeField]
+    public TextMeshProUGUI texto;
+
+    [SerializeField]
+    Sprite imgDefecto;
+
     private void Awake()
     {
         contenedorImagen = GameObject.Find("Contenedor");
@@ -23,6 +33,7 @@ public class Dialogo_SO : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        contenedorDialogo.SetActive(false);
         indexActual = -1;
     }
 
@@ -33,11 +44,13 @@ public class Dialogo_SO : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.F)) //atras
         {
+            contenedorDialogo.SetActive(true);
             if (indexActual > 0)
             {
                 indexActual--;
                 image.sprite = dialogo.getDatosPersonaje(indexActual).personaje.imagen;  //la forma más correcta es esta
                 //image.sprite = dialogo.mensaje[indexActual].personaje.imagen;
+                texto.text = dialogo.getDatosPersonaje(indexActual).dialogo;
             }
             else {
                     //de momento, no se hace nada
@@ -46,15 +59,50 @@ public class Dialogo_SO : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.G)) //adelante
         {
+            contenedorDialogo.SetActive(true);
             if (indexActual < dialogo.getCantidadMensajes()-1)
             {
                 indexActual++;
                 image.sprite = dialogo.getDatosPersonaje(indexActual).personaje.imagen;
+                texto.text = dialogo.getDatosPersonaje(indexActual).dialogo;
+
+                texto.richText = true;
+                texto.maxVisibleCharacters = 0;
+                StopAllCoroutines();
+                StartCoroutine("mostrarTexto");
             }
             else {
                 //de momento, no se hace nada
             }
         }
 
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            contenedorDialogo.SetActive(false);
+            indexActual = -1;
+            texto.text =  "Sin Texto";
+            image.sprite = imgDefecto;
+        }
+
     }
+
+    IEnumerator mostrarTexto()
+    {
+        while (true)
+        {
+            //Tarea para 19 octubre : 1 . detener corrutina una vez que termina su objetivo
+             //                       2 . asegurarse de no poder mostrar un mensaje hasta que el anterior   
+             //                           haya sido mostrado completamente
+
+            float largo = texto.text.Length;
+            if (texto.maxVisibleCharacters < largo)
+            {
+                texto.maxVisibleCharacters += 1;
+            }
+            Debug.Log("Ejecucíón Corrutina");
+            yield return new WaitForSeconds(.05f);
+        }
+    }
+
+
 }
